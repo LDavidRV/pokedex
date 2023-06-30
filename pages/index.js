41210c3ceeedae6bbee572059a62c7abdb22cfc7
david <ldramirezv@hotmail.com>
2023-06-29 21:36:38 -0500

    fixed search bar, load more button and deleted dark mode

--- pages/index.js
+++ pages/index.js
@@ -6,8 +6,6 @@ import styles from "../styles/Home.module.css";
 const Home = () => {
   const [pokemonList, setPokemonList] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
-  const [isLoading, setIsLoading] = useState(true);
-  const [darkMode, setDarkMode] = useState(false);
   const router = useRouter();
 
   useEffect(() => {
@@ -16,22 +14,19 @@ const Home = () => {
 
   const fetchPokemonList = async () => {
     try {
-      const response = await fetch(
-        "https://pokeapi.co/api/v2/pokemon?limit=20"
-      );
+      const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=1000");
       const data = await response.json();
       setPokemonList(data.results);
-      setIsLoading(false);
     } catch (error) {
       console.log(error);
     }
   };
-  
 
   const handleLoadMore = async () => {
     try {
+      const offset = pokemonList.length;
       const response = await fetch(
-        "https://pokeapi.co/api/v2/pokemon?offset=20&limit=20"
+        `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=20`
       );
       const data = await response.json();
       setPokemonList((prevList) => [...prevList, ...data.results]);
@@ -41,6 +36,10 @@ const Home = () => {
   };
 
   const handleSearch = (e) => {
+    const searchTerm = e.target.value.toLowerCase();
+    const filteredPokemonList = pokemonList.filter((pokemon) =>
+      pokemon.name.toLowerCase().includes(searchTerm)
+    );
     setSearchTerm(e.target.value);
   };
 
@@ -52,25 +51,13 @@ const Home = () => {
     pokemon.name.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
-  const toggleDarkMode = () => {
-    setDarkMode(!darkMode);
-  };
-
   return (
-    <div
-      className={`${styles.container} ${
-        darkMode ? styles.darkMode : styles.lightMode
-      }`}
-    >
+    <div className={styles.container}>
       <Head>
         <title>Pokédex</title>
       </Head>
 
       <h1 className={styles.title}>Pokédex</h1>
-
-      {/* <button className={styles.darkModeButton} onClick={toggleDarkMode}>
-        {darkMode ? "Light Mode" : "Dark Mode"}
-      </button> */}
 
       <input
         type="text"
@@ -80,32 +67,26 @@ const Home = () => {
         className={styles.searchInput}
       />
 
-      {isLoading ? (
-        <div>Loading...</div>
-      ) : (
-        <>
-          <div className={styles.pokemonList}>
-            {filteredPokemonList.map((pokemon) => (
-              <div key={pokemon.name} className={styles.pokemonCard}>
-                <img
-                  src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
-                    pokemon.url.split("/")[6]
-                  }.png`}
-                  alt={pokemon.name}
-                  onClick={() => handlePokemonClick(pokemon.url.split("/")[6])}
-                  className={styles.pokemonImage}
-                />
-                <p className={styles.pokemonName}>{pokemon.name}</p>
-              </div>
-            ))}
+      <div className={styles.pokemonList}>
+        {filteredPokemonList.slice(0, 20).map((pokemon) => (
+          <div key={pokemon.name} className={styles.pokemonCard}>
+            <img
+              src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+                pokemon.url.split("/")[6]
+              }.png`}
+              alt={pokemon.name}
+              onClick={() => handlePokemonClick(pokemon.url.split("/")[6])}
+              className={styles.pokemonImage}
+            />
+            <p className={styles.pokemonName}>{pokemon.name}</p>
           </div>
+        ))}
+      </div>
 
-          {pokemonList.length >= 20 && (
-            <button className={styles.loadMoreButton} onClick={handleLoadMore}>
-              Load More
-            </button>
-          )}
-        </>
+      {filteredPokemonList.length > 20 && (
+        <button className={styles.loadMoreButton} onClick={handleLoadMore}>
+          Load More
+        </button>
       )}
     </div>
   );
